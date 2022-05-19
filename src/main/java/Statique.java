bf35fa486443f45fd4edbe2cae6a268e8372fe0d
jorisSchaller <joris.schaller@heig-vd.ch>
2022-05-19 13:39:45 +0200

    Rajout du main original

--- src/main/java/Statique.java
+++ src/main/java/Statique.java
@@ -1,44 +1,23 @@
-// import commands.*;
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
-//
-// import java.util.concurrent.Callable;
-//
-// @Command(
-//        name = "statique",
-//        description = "A brand new static site generator.",
-//        subcommands = {Init.class, Clean.class, Build.class, Serve.class, Version.class})
-// public class Statique implements Callable<Integer> {
-//    public static void main(String... args) {
-//        int exitCode = new CommandLine(new Statique()).execute(args);
-//        if (exitCode != 0) {
-//            System.exit(exitCode);
-//        }
-//    }
-//
-//    @Override
-//    public Integer call() throws Exception {
-//        CommandLine.usage(this, System.out);
-//        return 0;
-//    }
-// }
-
-import java.nio.file.Path;
+import commands.*;
 import java.util.concurrent.Callable;
-import utils.Watcher;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
+@Command(
+        name = "statique",
+        description = "A brand new static site generator.",
+        subcommands = {Init.class, Clean.class, Build.class, Serve.class, Version.class})
 public class Statique implements Callable<Integer> {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Watcher watcher = new Watcher();
-        var s = new Statique();
-        watcher.register(s, Path.of("testWatch"));
-        System.out.println("Watching...");
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Statique()).execute(args);
+        if (exitCode != 0) {
+            System.exit(exitCode);
+        }
     }
 
     @Override
     public Integer call() throws Exception {
-        System.out.println("Callback called !");
+        CommandLine.usage(this, System.out);
         return 0;
     }
 }
