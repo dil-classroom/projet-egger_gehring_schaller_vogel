c2db31d95fcaffbb0948e153f4b51aa21c3275bf
jorisSchaller <joris.schaller@heig-vd.ch>
2022-05-19 13:29:43 +0200

    Ajout d'une class Watcher permettant d'être appelé lors d'un changement dans un repertoire spécifé

--- src/main/java/Statique.java
+++ src/main/java/Statique.java
@@ -1,24 +1,44 @@
-import commands.*;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
+// import commands.*;
+// import picocli.CommandLine;
+// import picocli.CommandLine.Command;
+//
+// import java.util.concurrent.Callable;
+//
+// @Command(
+//        name = "statique",
+//        description = "A brand new static site generator.",
+//        subcommands = {Init.class, Clean.class, Build.class, Serve.class, Version.class})
+// public class Statique implements Callable<Integer> {
+//    public static void main(String... args) {
+//        int exitCode = new CommandLine(new Statique()).execute(args);
+//        if (exitCode != 0) {
+//            System.exit(exitCode);
+//        }
+//    }
+//
+//    @Override
+//    public Integer call() throws Exception {
+//        CommandLine.usage(this, System.out);
+//        return 0;
+//    }
+// }
 
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
+import utils.Watcher;
 
-@Command(
-        name = "statique",
-        description = "A brand new static site generator.",
-        subcommands = {Init.class, Clean.class, Build.class, Serve.class, Version.class})
 public class Statique implements Callable<Integer> {
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new Statique()).execute(args);
-        if (exitCode != 0) {
-            System.exit(exitCode);
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Watcher watcher = new Watcher();
+        var s = new Statique();
+        watcher.register(s, Path.of("testWatch"));
+        System.out.println("Watching...");
     }
 
     @Override
     public Integer call() throws Exception {
-        CommandLine.usage(this, System.out);
+        System.out.println("Callback called !");
         return 0;
     }
 }
